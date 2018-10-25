a55fa641b015e0170c2df80f7c2c9bfa67314bde
joemac96 <maclennan.joe@gmail.com>
2018-10-25 15:03:22 -0400

    added MasterDetailPage

--- DrinkingBuddies/DrinkingBuddies/DrinkingBuddies/MainPage.xaml.cs
+++ DrinkingBuddies/DrinkingBuddies/DrinkingBuddies/MainPage.xaml.cs
@@ -1,22 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace DrinkingBuddies
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage : MasterDetailPage
 	{
-		public MainPage()
+		public MainPage ()
 		{
-			InitializeComponent();
+			InitializeComponent ();
+			masterPage.listView.ItemSelected += OnItemSelected;
+			if (Device.RuntimePlatform == Device.UWP)
+			{
+				MasterBehavior = MasterBehavior.Popover;
+			}
 		}
 
-		private void OnClick(object sender, EventArgs e)
+		private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
 		{
-			friendsLabel.IsVisible = true;
+			var item = e.SelectedItem as MasterPageItem;
+			if (item != null)
+			{
+				Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+				masterPage.listView.SelectedItem = null;
+				IsPresented = false;
+			}
 		}
 	}
 }
